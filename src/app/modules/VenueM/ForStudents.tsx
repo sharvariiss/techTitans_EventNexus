11dab7e01760c9e9068c9de59cc6eab206f8fc59
Sakshi Musmude <101097073+SakshiM1291@users.noreply.github.com>
2024-02-25 11:19:13 +0530

    Action Register

--- src/app/modules/VenueM/ForStudents.tsx
+++ src/app/modules/VenueM/ForStudents.tsx
@@ -1,6 +1,6 @@
-import React, {useState, ChangeEvent} from 'react'
-import {KTIcon} from '../../../_metronic/helpers'
-import {Link} from 'react-router-dom'
+import React, { useState, ChangeEvent } from 'react'
+import { KTIcon } from '../../../_metronic/helpers'
+import { Link } from 'react-router-dom'
 
 interface User {
   SrNo: string
@@ -59,46 +59,74 @@ const ForStudents: React.FC = () => {
     }
   }
 
-  const handleSelectAll = (event: ChangeEvent<HTMLInputElement>) => {
-    if (event.target.checked) {
-      const allUserIds = usersData.map((user) => user.SrNo)
-      setSelectedUsers(allUserIds)
-    } else {
-      setSelectedUsers([])
-    }
-  }
+  // const handleSelectAll = (event: ChangeEvent<HTMLInputElement>) => {
+  //   if (event.target.checked) {
+  //     const allUserIds = usersData.map((user) => user.SrNo)
+  //     setSelectedUsers(allUserIds)
+  //   } else {
+  //     setSelectedUsers([])
+  //   }
+  // }
 
-  const handleDeleteSelected = () => {
-    // Show confirmation dialog before deletion
-    setShowConfirmation(true)
-  }
+  // const handleDeleteSelected = () => {
+  //   // Show confirmation dialog before deletion
+  //   setShowConfirmation(true)
+  // }
 
-  const handleConfirmDelete = () => {
-    // Log the deleted users to the console
-    const deletedUsers = usersData.filter((user) => selectedUsers.includes(user.SrNo))
-    console.log('Deleted Users:', deletedUsers)
+  // const handleConfirmDelete = () => {
+  //   // Log the deleted users to the console
+  //   const deletedUsers = usersData.filter((user) => selectedUsers.includes(user.SrNo))
+  //   console.log('Deleted Users:', deletedUsers)
 
-    // Update usersData by removing selected users
-    const updatedUsersData = usersData.filter((user) => !selectedUsers.includes(user.SrNo))
+  //   // Update usersData by removing selected users
+  //   const updatedUsersData = usersData.filter((user) => !selectedUsers.includes(user.SrNo))
 
-    // Update the state, clear selection, and hide confirmation dialog
-    setSelectedUsers([])
-    usersData.splice(0, usersData.length, ...updatedUsersData)
-    setShowConfirmation(false)
-  }
+  //   // Update the state, clear selection, and hide confirmation dialog
+  //   setSelectedUsers([])
+  //   usersData.splice(0, usersData.length, ...updatedUsersData)
+  //   setShowConfirmation(false)
+  // }
 
-  const handleCancelDelete = () => {
-    // Clear selection and hide confirmation dialog
-    setSelectedUsers([])
-    setShowConfirmation(false)
-  }
+  // const handleCancelDelete = () => {
+  //   // Clear selection and hide confirmation dialog
+  //   setSelectedUsers([])
+  //   setShowConfirmation(false)
+  // }
 
-  const handleDeleteRow = (userId: string) => {
-    // Show confirmation dialog before deletion
-    setSelectedUsers([userId]) // Select the current row
-    setShowConfirmation(true)
-    // Log a message to the console
-  }
+  // const handleDeleteRow = (userId: string) => {
+  //   // Show confirmation dialog before deletion
+  //   setSelectedUsers([userId]) // Select the current row
+  //   setShowConfirmation(true)
+  //   // Log a message to the console
+  // }
+
+  const handleRegisterClick = () => {
+    // Show the registration confirmation popup
+    setShowConfirmation(true);
+  };
+
+  const handleConfirmRegistration = () => {
+    // Handle the registration confirmation logic here
+    // For example, you can navigate to a registration page
+    console.log('User confirmed registration');
+    // Close the confirmation popup
+    setShowConfirmation(false);
+  };
+
+  const handleCancelRegistration = () => {
+    // Close the confirmation popup without registering
+    setShowConfirmation(false);
+  };
+
+  const handleFeedbackChange = (event: ChangeEvent<HTMLInputElement>, userSrNo: string) => {
+    // Handle feedback change and update the user's feedback in the state or elsewhere
+    // For example, you can maintain feedback in the component's state
+    const updatedUsersData = usersData.map((user) =>
+      user.SrNo === userSrNo ? { ...user, feedback: event.target.value } : user
+    );
+    // Update the usersData state or perform other actions with the updated data
+    console.log('Updated Users Data:', updatedUsersData);
+  };
 
   return (
     <>
@@ -239,8 +267,30 @@ const ForStudents: React.FC = () => {
                       <td>{user.Department}</td>
                       <td>{user.date}</td>
                       <td>{user.time}</td>
-                      <td>{user.action}</td>
-                      <td>{user.feedback}</td>
+                      <td>
+                        {user.action === 'Register' && (
+                          <button
+                            className='btn btn-primary btn-sm'
+                            onClick={handleRegisterClick}
+                          >
+                            {user.action}
+                          </button>
+                        )}
+                      </td>
+                      <td>
+                        <input
+                          type='text'
+                          value={user.feedback}
+                          onChange={(e) => handleFeedbackChange(e, user.SrNo)}
+                          style={{
+                            width: '100px',
+                            borderRadius: '5px',
+                            padding: '5px',
+                            border: '1px solid grey'
+                          }}
+                        />
+                      </td>
+                      {/* <td>{user.feedback}</td> */}
                       {/* <td>{user.totalAmount}</td>
                       <td>{user.paid}</td>
                       <td>{user.remaining}</td> */}
@@ -274,6 +324,7 @@ const ForStudents: React.FC = () => {
           </div>
         </div>
       </div>
+
       {/* Tab section */}
       <ul className='nav nav-stretch nav-line-tabs nav-line-tabs-2x border-transparent fs-5 fw-bolder mb-5'>
         <li className='nav-item'>
@@ -292,6 +343,41 @@ const ForStudents: React.FC = () => {
           </a>
         </li>
       </ul>
+      {showConfirmation && (
+        <div className='modal fade show' style={{ display: 'block' }}>
+          <div className='modal-dialog'>
+            <div className='modal-content'>
+              <div className='modal-header'>
+                <h5 className='modal-title'>Registration Confirmation</h5>
+                <button
+                  type='button'
+                  className='btn-close'
+                  onClick={handleCancelRegistration}
+                ></button>
+              </div>
+              <div className='modal-body'>
+                <p>Do you want to participate in this event?</p>
+              </div>
+              <div className='modal-footer'>
+                <button
+                  type='button'
+                  className='btn btn-primary'
+                  onClick={handleConfirmRegistration}
+                >
+                  Yes
+                </button>
+                <button
+                  type='button'
+                  className='btn btn-secondary'
+                  onClick={handleCancelRegistration}
+                >
+                  No
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
       {/* Tab content */}
       <div className='tab-content'>
@@ -314,4 +400,4 @@ const ForStudents: React.FC = () => {
   )
 }
 
-export {ForStudents}
+export { ForStudents }
